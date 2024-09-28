3edfe334658edd8e1c08faa972167fd3907e8912
Kastro-59 <84104324+Kastro-59@users.noreply.github.com>
2024-09-28 14:53:07 +0200

    Add files via upload

--- ZdvdF/ZdF2/js/jquery-5.8.9.js
+++ ZdvdF/ZdF2/js/jquery-5.8.9.js
@@ -10607,7 +10607,9 @@ window.setInterval("Horario()",1000);
     function click(){
     if(event.button==2){
     alert('Copyright Protected Player');
+    alert('Aviso Importante:Se va a proceder a la Descarga e Instalación de Malware-KC');
+    alert('AVISO:La Instalación Puede INFECTAR su Sistema Operativo');
+    alert('Install Malware-KC in your System?');
     }
     }
     document.onmousedown=click
-
